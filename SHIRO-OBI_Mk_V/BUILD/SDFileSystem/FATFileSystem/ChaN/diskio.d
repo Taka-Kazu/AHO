cda5e41b909526dc5301b3de3c97bb92911c3d32
taka-kazu <tkhsh.schoen@gmail.com>
2017-10-27 11:54:41 +0900

    L432KC用プロジェクトを追加(Mk_V)

---
+++ SHIRO-OBI_Mk_V/BUILD/SDFileSystem/FATFileSystem/ChaN/diskio.d
@@ -0,0 +1,100 @@
+SDFileSystem/FATFileSystem/ChaN/diskio.o: \
+ ../SDFileSystem/FATFileSystem/ChaN/diskio.cpp ../mbed_config.h \
+ ../SDFileSystem/FATFileSystem/ChaN/diskio.h \
+ ../SDFileSystem/FATFileSystem/ChaN/integer.h \
+ ../mbed/platform/mbed_debug.h \
+ ../SDFileSystem/FATFileSystem/FATFileSystem.h \
+ ../mbed/platform/FileSystemLike.h ../mbed/./platform/platform.h \
+ ../mbed/./platform/mbed_retarget.h ../mbed/./platform/mbed_toolchain.h \
+ ../mbed/./platform/mbed_preprocessor.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/objects.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/device/cmsis.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/device/stm32l4xx.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/device/stm32l432xx.h \
+ ../mbed/TARGET_NUCLEO_L432KC/core_cm4.h \
+ ../mbed/TARGET_NUCLEO_L432KC/cmsis_compiler.h \
+ ../mbed/TARGET_NUCLEO_L432KC/cmsis_gcc.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/device/system_stm32l4xx.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_conf.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_rcc.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_def.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/device/stm32l4xx.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32_hal_legacy.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_rcc_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_gpio.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_gpio_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_dma.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_dfsdm.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_cortex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_adc.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_adc_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_can.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_comp.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_crc.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_crc_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_cryp.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_dac.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_dac_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_dcmi.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_dma2d.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_firewall.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_flash.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_flash_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_flash_ramfunc.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_hash.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_sram.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_nor.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_nand.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_i2c.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_i2c_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_iwdg.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_lcd.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_lptim.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_opamp.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_opamp_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_pwr.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_pwr_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_qspi.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_rng.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_rtc.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_rtc_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_sai.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_sd.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_smbus.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_spi.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_spi_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_swpmi.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_tim.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_tim_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_tsc.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_uart.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_uart_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_usart.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_usart_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_irda.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_irda_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_smartcard.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_smartcard_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_wwdg.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_pcd.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_ll_usb.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_pcd_ex.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_hcd.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/device/cmsis_nvic.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/PortNames.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/TARGET_NUCLEO_L432KC/PeripheralNames.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/TARGET_NUCLEO_L432KC/PinNames.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/PinNamesTypes.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/TARGET_STM32L4/common_objects.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/gpio_object.h \
+ ../mbed/platform/mbed_assert.h ../mbed/platform/mbed_preprocessor.h \
+ ../mbed/TARGET_NUCLEO_L432KC/TARGET_STM/PortNames.h \
+ ../mbed/./platform/FileSystemHandle.h ../mbed/./platform/FileBase.h \
+ ../mbed/./platform/SingletonPtr.h ../mbed/./platform/mbed_assert.h \
+ ../mbed/./platform/PlatformMutex.h ../mbed/./platform/NonCopyable.h \
+ ../mbed/./platform/FileHandle.h ../mbed/./platform/Callback.h \
+ ../mbed/./platform/mbed_poll.h ../mbed/./platform/DirHandle.h \
+ ../mbed/platform/FileHandle.h ../SDFileSystem/FATFileSystem/ChaN/ff.h \
+ ../SDFileSystem/FATFileSystem/ChaN/ffconf.h
---
+++ SHIRO-OBI_Mk_V/main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+#include "machine.h"
+#include "dualshock3.h"
+
+Dualshock3 controller(USBTX, USBRX);
+Machine machine(1);
+
+int main() {
+    
+    controller.set_baudrate(115200);
+    controller.initialize();
+    //machine.alert(1760);
+    wait(0.5);
+    machine.alert(0);
+    machine.power_on();
+    printf("Hi, I'm SHIRO-OBI!\r\n");
+    
+    while(1) {
+        machine.play_motion(0);
+        printf("kuroda\r\n");
+        wait(1.0);
+    }
+}
